86acf7ba82203e1134859c16d77f350dcdaaa220
Hasan <hasan.doctype@gmail.com>
2021-10-14 20:44:18 +0400

    add JS Lesson1 noscript documentWrite

---
+++ JSLesson/VanilleJS/Lesson1/script.js
@@ -0,0 +1,33 @@
+        /*
+        more line comment
+        */
+        //one line comment
+        // document.write('Hello JavaScript');
+        // document.write(' Hello JavaScript');
+
+        // document.writeln("New Line");
+        // document.writeln("New Line");
+
+        // alert("Hello");
+        // window.alert("Hello1")
+
+        // console.log("Hello JS")
+
+        // debugger;
+
+        // console.log("Hello1");
+        // console.log("Hello2");
+        // console.log("Hello3");
+        // console.log("Hello4");
+        // console.log("Hello5");
+        // console.log("Hello6");
+        // console.log("Hello7");
+        // console.log("Hello8");
+        // console.log("Hello9");
+        // console.log("Hello10");
+        // console.log("Hello11");
+        // console.log("Hello12");
+        // console.log("Hello13");
+        // console.log("Hello14");
+        // console.loag("Hello15");
+        // console.log("Hello16");

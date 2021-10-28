a4686558ce8af55176ea4bb29e5e9ded3c3ce3e7
Hasan Bakhtiar <hasan.doctype@gmail.com>
2021-10-28 20:32:30 +0400

    add JS Lesson 5

---
+++ JSLesson/VanilleJS/Lesson5/main.js
@@ -0,0 +1,33 @@
+// while
+
+// let a = 1;
+
+// while (a <= 100) {
+//    console.log(a);
+//     a++;
+// }
+
+
+
+
+// do while
+// let a = 1;
+
+// do{
+//     console.log(a);
+//     a++;
+// }while(a > 100);
+
+// for 
+
+// for(let a = 1; a<100; a++){
+//     console.log(a);
+// }
+
+// for in
+
+let a = ["Hasim","Lale","Kamran"];
+
+for( x in a){
+    console.log(a[x]);
+}

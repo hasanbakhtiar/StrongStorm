2a09d20667ff795211154d3849d2ab8f35c054de
Hasan Bakhtiar <hasan.doctype@gmail.com>
2021-11-19 15:05:29 +0400

    todo

--- JSLesson/VanilleJS/Lesson10-BasicToDo/js/main.js
+++ JSLesson/VanilleJS/Lesson10-BasicToDo/js/main.js
@@ -1,6 +1,7 @@
 const addToDoButton = document.querySelector("#addToDo");
 const inputField = document.querySelector("#inputField");
 const toDoContainer = document.querySelector("#toDoContainer");
+
 const addElement = ()=>{
 
     let paragrph = document.createElement('p');

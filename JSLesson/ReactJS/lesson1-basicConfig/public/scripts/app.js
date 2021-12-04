c0086d61cda4dc38dac345042ac8c01f082d84ee
Hasan Bakhtiar <52384595+HasanBakhtiar@users.noreply.github.com>
2021-12-04 23:32:27 +0400

    react last

---
+++ JSLesson/ReactJS/lesson1-basicConfig/public/scripts/app.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var temp = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("p", {
+  style: {
+    'color': 'red'
+  }
+}, "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo doloribus temporibus deleniti maxime. Accusamus atque et repudiandae cum amet, veniam quo debitis dolorum labore quae accusantium velit eveniet nobis saepe?"), /*#__PURE__*/React.createElement("p", null, "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo doloribus temporibus deleniti maxime. Accusamus atque et repudiandae cum amet, veniam quo debitis dolorum labore quae accusantium velit eveniet nobis saepe?"), /*#__PURE__*/React.createElement("p", null, "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo doloribus temporibus deleniti maxime. Accusamus atque et repudiandae cum amet, veniam quo debitis dolorum labore quae accusantium velit eveniet nobis saepe?"), /*#__PURE__*/React.createElement("p", null, "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo doloribus temporibus deleniti maxime. Accusamus atque et repudiandae cum amet, veniam quo debitis dolorum labore quae accusantium velit eveniet nobis saepe?"), /*#__PURE__*/React.createElement("h1", null, "dskdjdfjkds"));
+ReactDOM.render(temp, document.querySelector("#root"));
---
+++ JSLesson/ReactJS/lesson1-basicConfig/src/app.js
@@ -0,0 +1,24 @@
+
+const temp =(
+    <div>
+        <p style={{'color':'red'}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo doloribus temporibus deleniti maxime.
+             Accusamus atque et repudiandae cum amet, veniam quo debitis dolorum labore quae accusantium velit 
+             eveniet nobis saepe?</p>
+
+             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo doloribus temporibus deleniti maxime.
+             Accusamus atque et repudiandae cum amet, veniam quo debitis dolorum labore quae accusantium velit 
+             eveniet nobis saepe?</p>
+
+             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo doloribus temporibus deleniti maxime.
+             Accusamus atque et repudiandae cum amet, veniam quo debitis dolorum labore quae accusantium velit 
+             eveniet nobis saepe?</p>
+
+
+             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo doloribus temporibus deleniti maxime.
+             Accusamus atque et repudiandae cum amet, veniam quo debitis dolorum labore quae accusantium velit 
+             eveniet nobis saepe?</p>
+             <h1>dskdjdfjkds</h1>
+    </div>
+)
+
+ReactDOM.render(temp,document.querySelector("#root"))

6c5a4dd2647fe4ac3d4b700356e6d90509512bb2
Hasan Bakhtiar <hasan.doctype@gmail.com>
2021-12-07 15:16:58 +0400

    basicConfig1

---
+++ JSLesson/ReactJS/lesson2-basicConfig2/public/scripts/app.js
@@ -0,0 +1,5 @@
+"use strict";
+
+var temp = /*#__PURE__*/React.createElement("h1", null, "Hello");
+var root = document.querySelector('#root');
+ReactDOM.render(temp, root);
---
+++ JSLesson/ReactJS/lesson2-basicConfig2/src/app.js
@@ -0,0 +1,6 @@
+const temp = (
+    <h1>Hello</h1>
+) 
+
+const root = document.querySelector('#root');
+ReactDOM.render(temp,root);

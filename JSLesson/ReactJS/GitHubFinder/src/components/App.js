2c79713c37a70bb7ee605eb302bc00333ba09d76
Hasan Bakhtiar <hasan.doctype@gmail.com>
2022-01-06 21:04:30 +0400

    githubFinder

---
+++ JSLesson/ReactJS/GitHubFinder/src/components/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import Navbar from './Navbar';
+import Search from './Search';
+import Users from './Users';
+import axios from 'axios';
+
+class App extends Component {
+    constructor(props){
+        super(props);
+        this.searchUsers = this.searchUsers.bind(this);
+        this.state = {
+            users: []
+        }
+    }
+
+    searchUsers(keyword){
+        axios
+        .get(`https://api.github.com/search/users?q=${keyword}`)
+        .then(res => this.setState({users:res.data.items}))
+
+    }
+    render() {
+        return (
+            <div>
+                <Navbar url="https://github.com" icon="fab fa-github" title=" Github" />
+                <Search  searchUsers={this.searchUsers}/>
+                <Users users={this.state.users}/>
+            </div>
+        )
+    }
+}
+
+export default App
